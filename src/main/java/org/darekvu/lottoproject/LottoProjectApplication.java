4601a0a2a59e3def421f09bc0d45a5581b3c1540
Quang Du Vu (Darek) <vuquang.dd@gmail.com>
2024-09-02 18:36:18 +0200

    initial commit

---
+++ src/main/java/org/darekvu/lottoproject/LottoProjectApplication.java
@@ -0,0 +1,13 @@
+package org.darekvu.lottoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoProjectApplication.class, args);
+	}
+
+}
