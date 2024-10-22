cdd4f3b5e1821622546d540a252ec47868c40046
Quang Du Vu (Darek) <vuquang.dd@gmail.com>
2024-10-22 20:18:14 +0200

    adding dockercompose for mongodb and connecting mongodb with the spring boot

---
+++ src/integration/java/org/lottoproject/feature/HappyPathIntegrationTest.java
@@ -0,0 +1,11 @@
+package org.lottoproject.feature;
+
+import org.junit.jupiter.api.Test;
+import org.lottoproject.BaseIntegrationTest;
+
+class HappyPathIntegrationTest extends BaseIntegrationTest {
+    @Test
+    public void f() {
+
+    }
+}
